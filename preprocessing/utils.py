547d6acc296cb66267613601521754d67e5768a4
piesauce <15797780+piesauce@users.noreply.github.com>
2019-08-15 23:20:33 -0400

    Update utils.py

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -22,3 +22,25 @@ def merge_plays(path):
             with open(fname) as infile:
                 for line in infile:
                     outfile.write(line)
+                    
+def merge_plays(path):
+    play_count = 0
+    col_names = ['text', 'playid', 'style']
+    df = pd.DataFrame(columns=col_names)
+    for dirname, _, filenames in os.walk(path):
+        for filename in filenames:
+            if 'original' in filename:
+                style = 0
+            if 'modern' in filename:
+                style = 1
+            for line in open(os.path.join(dirname, filename)):
+                df.loc[len(df)] = [line, play_count, style]
+            play_count += 1
+    return df
+    
+def save_df(df):
+    df.to_pickle("./plays_df.pkl")
+    
+def load_df(path):
+    return pd.read_pickle(path)
+    
